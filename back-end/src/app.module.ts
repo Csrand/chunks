d6cf629e483e5bd88fe01caa0179a44823ba30f1
Csrand <chrystiansenna@gmail.com>
2025-11-11 11:30:19 -0300

    fazendo o que posso, quando posso.

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DisciplineModule } from './discipline/discipline.module';
-
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 @Module({
-  imports: [DisciplineModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+
+
+      })
+    }),
+    DisciplineModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- back-end/src/discipline/entities/discipline.entity.ts
+++ back-end/src/discipline/entities/discipline.entity.ts
@@ -1,1 +1,28 @@
-export class Discipline {}
+import { BaseEntity } from 'src/commons/entity/base.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('BD2_DISCIPLINES')
+export class Discipline extends BaseEntity{
+    @PrimaryGeneratedColumn('increment', {
+        name: 'DISCIPLINE_ID',
+        type: 'number',
+    })
+    disciplineId: number;
+
+    @Column({
+        name: 'NAME',
+        type: 'varchar2',
+    })
+    disciplineName: string;
+
+    @Column({
+        name: 'DESCRIPTION',
+        type: 'text',
+    })
+    discipleDescription: string;
+
+    constructor(data: Partial<Discipline> = {}){
+        super();
+        Object.assign(this, data);
+    }
+}
